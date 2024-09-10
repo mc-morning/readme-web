b1189f71039ac6817c9b5be629588fddc8336c60
jeonkyungwon <ashjkw3218@gmail.com>
2024-09-10 17:08:19 +0900

    FEAT : Main & Login Page UI 구현

--- src/routes/FormList.jsx
+++ src/routes/FormList.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import styled from "styled-components";
+
 function FormList() {
     return (
         <div>답변 리스트 페이지</div>
--- src/routes/WriteAnswer.jsx
+++ src/routes/WriteAnswer.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import styled from "styled-components";
+
 function WriteAnswer() {
     return (
         <div>답변 작성 페이지</div>
