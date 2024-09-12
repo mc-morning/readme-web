2ab94c4e4507eb5931dc010e5673d96f114f4ba3
jeonkyungwon <ashjkw3218@gmail.com>
2024-09-12 17:16:33 +0900

    FEAT : 질문 생성 페이지 UI 보안 & 질문지 완성 페이지 UI 구현

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Main from "./routes/Main/Main";
 import Login from "./routes/Login/Login";
-import CreateForm from "./routes/CreateForm/CreateForm";
+import CreateForm from "./routes/CreateForm/Create/CreateForm";
 import WriteAnswer from "./routes/WriteAnswer";
 import FormList from "./routes/FormList";
 import Nav from "./components/Nav/Nav";
+import CompleteForm from "./routes/CreateForm/Complete/CompleteForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Main />}></Route>{" "}
         <Route path="login" element={<Login />}></Route>
         <Route path="createform" element={<CreateForm />}></Route>
+        <Route path="completeform" element={<CompleteForm />}></Route>
         <Route path="writeanswer" element={<WriteAnswer />}></Route>
         <Route path="formlist" element={<FormList />}></Route>
       </Routes>
