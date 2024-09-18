2973efcef31078a4dc328e0594899ab843dc51b4
jeonkyungwon <ashjkw3218@gmail.com>
2024-09-18 23:09:25 +0900

    FEAT : 답변 작성 페이지 UI 구현

---
+++ src/components/Answer/Write/FormTitle.styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const TitleBox = styled.div`
+  width: 920px;
+  color: #333;
+  font-family: "Pretendard Variable";
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 600;
+  padding: 45px 0 25px 0;
+`;
+
+export const Desc = styled.p`
+  color: #707070;
+  font-family: "Pretendard Variable";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  line-height: 150%;
+`;
