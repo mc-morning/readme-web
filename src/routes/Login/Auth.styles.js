bac33a71abfa3125cd3405bce56d3a1b453f37b2
jeonkyungwon <ashjkw3218@gmail.com>
2024-09-20 17:22:21 +0900

    FEAT : 카카오 로그인 기능 구현

---
+++ src/routes/Login/Auth.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 92.3vh;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #333;
+  font-family: "Pretendard Variable";
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%;
+  gap: 15px;
+`;
